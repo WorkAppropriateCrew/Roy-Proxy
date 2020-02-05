3efcb9ba0697feccd60eec9b53b3aa6145339e58
RoyOlivarez <royolivarez66@gmail.com>
2020-02-05 17:05:48 -0600

    updating dependencies

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 8005 // updated from 3000
 const path = require("path");
 
+//middleware
+app.use(cors())
+app.use(express.static(path.join(__dirname, "/")));
 
-
-
-// This line is necessary to render React files
-app.use(express.static(path.join(__dirname, "/../client/dist")));
-
-app.get('/', (req, res) => res.send('proxy server'))
+// app.get('/', (req, res) => res.send('proxy server'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
