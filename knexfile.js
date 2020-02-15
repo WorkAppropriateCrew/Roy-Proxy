0a2d0893c0c0f571d70d614436404f8e73867022
RoyOlivarez <royolivarez66@gmail.com>
2020-02-15 11:04:26 -0600

    trying to update migrations 11:00 am

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,6 @@ module.exports = {
   development: {
       client: "postgresql",
       connection: 'postgres://postgres:password@localhost:5432/royolivarez',
-      // connection: {
-      //   host: 'localhost',
-      //   user: 'postgres',
-      //   password: 'Royoli1983',
-      //   database: 'SDC_PostgresDB'
-      // },
       migrations: {
           directory: __dirname + '/db/migrations',
         },
